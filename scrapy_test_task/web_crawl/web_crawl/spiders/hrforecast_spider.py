c509ced16efcae2ab67fe4316dee74a1d27abc66
FE1979 <ronin911@gmail.com>
2019-06-20 16:03:13 +0300

    HRForecast spider done

--- scrapy_test_task/web_crawl/web_crawl/spiders/hrforecast_spider.py
+++ scrapy_test_task/web_crawl/web_crawl/spiders/hrforecast_spider.py
@@ -13,8 +13,28 @@ class JobSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        """ Collects job urls an gets data from them
+        """
 
-        for job in response.css('h3 a::text').getall():
-            yield {
-                'Job title': job,
-            }
+        jobs = []
+        for job_url in response.css('h3 a::attr(href)').getall():
+            jobs.append(job_url)
+
+        for job in jobs:
+            yield response.follow(job, self.parse_job)
+
+    def parse_job(self, response):
+        """ Gets job description
+        """
+        job_response = response.css('div.container section.av_textblock_section')[0]
+        job_location = response.css('div.container section.av_textblock_section')[1]
+        job_location = job_location.css('::text').get()
+        job_data = job_response.css('::text').getall()
+
+        return {
+            'job_title': job_data[0],
+            'location': job_location,
+            'job_time': job_data[2],
+            'job_description': job_data[5:],
+            'job_url': response.url,
+        }

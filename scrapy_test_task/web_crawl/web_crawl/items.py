8710b61f964d8cfd3e0297ebbd42a514a28d77ae
FE1979 <ronin911@gmail.com>
2019-06-28 11:14:23 +0300

    Vacancy item defined. Added ItemLoader to HRforecast

--- scrapy_test_task/web_crawl/web_crawl/items.py
+++ scrapy_test_task/web_crawl/web_crawl/items.py
@@ -8,7 +8,17 @@
 import scrapy
 
 
-class WebCrawlItem(scrapy.Item):
+class VacancyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    count = scrapy.Field()
+    job_title = scrapy.Field()
+    post_date = scrapy.Field()
+    region = scrapy.Field()
+    employer = scrapy.Field()
+    location = scrapy.Field()
+    job_description = scrapy.Field()
+    job_requirements = scrapy.Field()
+    job_url = scrapy.Field()
+    scrap_date = scrapy.Field()
--- scrapy_test_task/web_crawl/web_crawl/spiders/hrforecast_spider.py
+++ scrapy_test_task/web_crawl/web_crawl/spiders/hrforecast_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 import time
+from scrapy.loader import ItemLoader
+from web_crawl.items import VacancyItem
 
 
 class JobSpider(scrapy.Spider):
@@ -27,16 +29,19 @@ class JobSpider(scrapy.Spider):
     def parse_job(self, response):
         """ Gets job description
         """
+
+        vacancy = ItemLoader(item=VacancyItem(), response=response)
+
         job_response = response.css('div.container section.av_textblock_section')[0]
         job_location = response.css('div.container section.av_textblock_section')[1]
         job_location = job_location.css('::text').get()
         job_data = job_response.css('::text').getall()
 
-        return {
-            'job_title': job_data[0],
-            'location': job_location,
-            'job_time': job_data[2],
-            'job_description': job_data[5:],
-            'job_url': response.url,
-            'scrap_date': time.strftime('%d/%m/%Y'),
-        }
+        vacancy.add_value('job_title', job_data[0])
+        vacancy.add_value('location', job_location)
+        vacancy.add_value('post_date', job_data[2])
+        vacancy.add_value('job_description', job_data[5:])
+        vacancy.add_value('job_url', response.url)
+        vacancy.add_value('scrap_date', time.strftime('%d/%m/%Y'))
+
+        return vacancy.load_item()
